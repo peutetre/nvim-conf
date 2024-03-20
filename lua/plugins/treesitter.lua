7b62b648f3017639a7b8472c25a68b34248bf5b6
Paul Panserrieu <paul.panserrieu@gmail.com>
2024-03-20 16:21:19 +0100

    fix conf

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return{
     end,
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = {"c", "python", "lua", "vim", "vimdoc", "toml", "bash", "bibtex", "css", "diff", "gitignore", "html", "javascript", "json", "css", "lua", "markdown", "swift", "typescript", "javascript", "tsx"},
+        ensure_installed = {"c", "python", "lua", "vim", "vimdoc", "toml", "bash", "bibtex", "css", "diff", "gitignore", "html", "javascript", "json", "css", "lua", "markdown", "typescript", "javascript", "tsx"},
         sync_install = false,
         auto_install = true,
         highlight = {
